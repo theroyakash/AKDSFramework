890dca0dbbb8b8287ef574794daa709adfa6b93e
theroyakash <business.theroyakash@outlook.com>
2020-11-01 00:52:17 +0530

    linked list added

---
+++ AKDSFramework/linkedlist.py
@@ -0,0 +1,90 @@
+"""
+This is the beginning of Linked list data structure module.
+We currently support singly linked list and doubly linked list.
+
+@author: theroyakash
+"""
+
+
+# Linked list helper class Node
+
+
+class Node:
+    def __init__(self, value):
+        """
+        Node class for linked list. Each element of a linked list is called a node.
+        Args:
+            value (Any): For each node this attribute will carry the value
+        """
+        self.value = value
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        """
+        Initialization of Singly linked list.
+        """
+        # Sentinel Means a soldier or guard whose job is to stand and keep watch.
+        self.sentinel = Node(None)
+        self.head = self.sentinel
+        self.size = 0
+
+    def add(self, value):
+        """
+        Adds any element to the linked list
+        Args:
+            value (Any): Put in the value you want to add.
+        """
+        new = Node(value)
+
+        if self.size == 0:
+            # If there exists no element in the LinkedList
+            self.head.next = new
+            self.head = new
+            self.size += 1
+        else:
+            # If there exists at least one/few element in the LinkedList
+            self.sentinel.next = new
+            new.next = self.head
+            self.head = new
+            self.size += 1
+
+    def get_head(self):
+        """
+        Get the head node value of the linked list
+        Returns:
+            - The node value for the head
+        """
+        return self.head.value
+
+    def count(self):
+        """
+        Get the size of the linked list. How many Nodes are there in the linked list.
+        Returns:
+            - The number of nodes in the linked list.
+        """
+        return self.size
+
+    def isEmpty(self):
+        """
+        Checks if linked list is empty or not.
+        Returns:
+            - Bool: Return True if linkedlist is empty else False.
+        """
+        return True if self.size == 0 else False
+
+    def __str__(self):
+        """
+        This is the string representation of the linked list values
+        Returns:
+            - String representation of the linked list values
+        """
+        array = []
+        current = self.head
+
+        while current:
+            array.append(current.value)
+            current = current.next
+
+        return str(array)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AKDSFramework
-Python Package for all your data structure and algorithm needs
+Python Package for all your data structure and algorithm needs.
+AKDSFramework is a Purely written in Python library containing implementations 
+of various data structures.
+
+Our Package will allow user to focus on developing algorithms 
+and not worry about finding python-compatible implementations of 
+classic data structures like linked list, heap and others.
+
+## Currently supporting
+- Python 3
+- and installation via `pip` package installer.
