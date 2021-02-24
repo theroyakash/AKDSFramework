a06230e39a768b409fa7811a9cf0ec9a97f5cacf
theroyakash <royakashappleid@icloud.com>
2021-02-25 00:03:48 +0530

    string representation updated, sparse_matrix started and few methods added.

--- AKDSFramework/error/__init__.py
+++ AKDSFramework/error/__init__.py
@@ -1,3 +1,3 @@
 from .error import HeapNotBuildError, NotFoundError, InvalidOperationError
 from .error import EmptyStackError, BadVertexTypeError, NegativeEdgeCycleWarning
-from .error import BadOrderingError
+from .error import BadOrderingError, NotValidMatrixError
